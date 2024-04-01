3e1e2a7a233b590e7ee15c9303e38795f6db75e4
s4r44d43 <sararez@live.it>
2024-04-01 17:14:18 +0200

    classes to fix

--- src/components/partials/Card.vue
+++ src/components/partials/Card.vue
@@ -13,12 +13,11 @@ export default {
 <template>
   <div class="col mb-3">
     <div class="mt-3">
-      <img :src="image" class="photo card-img-top" :alt="name">
-      <div class="card-body">
-        <h5 class="card-title">{{ name }}</h5>
-        <h5 class="card-title">----</h5>
-        <h6 class="card-title">{{ status }}</h6>
-        <p class="text-center">{{ species }}</p>
+      <img :src="image" class="card-img-top" :alt="name">
+      <div>
+        <h3>{{ name }}</h3>
+        <p>{{ status }}</p>
+        <p>{{ species }}</p>
       </div>
     </div>
   </div>
